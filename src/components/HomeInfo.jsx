36e06c7fab8bd3dcabe78979c0439a59c4eb7a6a
yk21ju@gmail.com <yk21ju@gmail.com>
2023-12-27 17:03:38 -0800

    first commit

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,66 @@
+import { Link } from "react-router-dom";
+
+import { arrow } from "../assets/icons";
+
+const HomeInfo = ({ currentStage }) => {
+  if (currentStage === 1)
+    return (
+      <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+        Hi, I'm
+        <span className='font-semibold mx-2 text-white'>Yong Elliot Kim</span>
+        👋
+        <br />
+        A Software Engineer from Seattle
+      </h1>
+    );
+
+  if (currentStage === 2) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium sm:text-xl text-center'>
+          Working towards becoming a full stack software developer <br />
+          I can pick up new skills and learn new technologies quickly!
+        </p>
+
+        <Link to='/about' className='neo-brutalism-white neo-btn'>
+          Learn more
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 3) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium text-center sm:text-xl'>
+          working on many projects to become successful. <br /> check out my portfolio
+        </p>
+
+        <Link to='/projects' className='neo-brutalism-white neo-btn'>
+          Visit my portfolio
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 4) {
+    return (
+      <div className='info-box'>
+      <p className='font-medium sm:text-xl text-center'>
+        Looking for a developer? <br/> I'm just a few keystrokes away
+      </p>
+
+      <Link to='/contact' className='neo-brutalism-white neo-btn'>
+        Let's talk
+        <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+      </Link>
+    </div>
+    );
+  }
+
+  return null;
+};
+
+export default HomeInfo;
