328580ad1986cbc5aeb534279d829679b0d2bd03
yk21ju@gmail.com <yk21ju@gmail.com>
2023-12-29 16:05:06 -0800

    second commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,25 @@
 const About = () => {
   return (
-    <div>About</div>
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm <span className="blue-gradient_text font-semibold drop-shadow-sm">Yong Elliot Kim</span>
+      </h1>
+
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+        Software Engineer in Seattle seeking developer roles, with a strong aptitude for quickly learning new technologies and skills.
+        </p>
+      </div>
+
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">
+          My Skills
+        </h3>
+        <div className="mt-16 flex flex-wrap gap-12">
+          
+        </div>
+      </div>
+    </section>
   )
 }
 
